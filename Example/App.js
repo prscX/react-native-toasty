8d8a98f9b081647109f3dfb521b63797a7e663fc
Pranav Chauhan <pranav.chauhan@metricstream.com>
2019-12-10 13:23:02 +0530

    - migrating to RN61+

--- Example/App.js
+++ Example/App.js
@@ -22,37 +22,37 @@ export default class App extends Component<Props> {
     let copy = <Icon name="copy" size={24} color="#FFFFFF" family={"FontAwesome"} />;
 
     return <View style={styles.container}>
-        <TouchableOpacity onPress={() => {
-            RNToasty.Normal({ title: "Message" });
-          }}>
-          <Text>{"Normal"}</Text>
-        </TouchableOpacity>
-        <TouchableOpacity onPress={() => {
-            RNToasty.Info({ title: "Message" });
-          }}>
-          <Text>{"Info"}</Text>
-        </TouchableOpacity>
-        <TouchableOpacity onPress={() => {
-          RNToasty.Success({ title: 'Message' })
-        }}>
-          <Text>{'Success'}</Text>
-        </TouchableOpacity>
-        <TouchableOpacity onPress={() => {
-          RNToasty.Warn({ title: 'Message' })
-        }}>
-          <Text>{'Warning'}</Text>
-        </TouchableOpacity>
-        <TouchableOpacity onPress={() => {
-          RNToasty.Error({ title: 'Message' })
-        }}>
-          <Text>{'Error'}</Text>
-        </TouchableOpacity>
-        <TouchableOpacity onPress={() => {
-          RNToasty.Show({ title: "Message", titleSize: 50, titleColor: '#555555', withIcon: true, duration: 1, tintColor: '#000000', icon: copy });
-        }}>
-          <Text>{'Custom'}</Text>
-        </TouchableOpacity>
-      </View>;
+      <TouchableOpacity onPress={() => {
+        RNToasty.Normal({ title: "Message" });
+      }}>
+        <Text>{"Normal"}</Text>
+      </TouchableOpacity>
+      <TouchableOpacity onPress={() => {
+        RNToasty.Info({ title: "Message" });
+      }}>
+        <Text>{"Info"}</Text>
+      </TouchableOpacity>
+      <TouchableOpacity onPress={() => {
+        RNToasty.Success({ title: 'Message' })
+      }}>
+        <Text>{'Success'}</Text>
+      </TouchableOpacity>
+      <TouchableOpacity onPress={() => {
+        RNToasty.Warn({ title: 'Message' })
+      }}>
+        <Text>{'Warning'}</Text>
+      </TouchableOpacity>
+      <TouchableOpacity onPress={() => {
+        RNToasty.Error({ title: 'Message' })
+      }}>
+        <Text>{'Error'}</Text>
+      </TouchableOpacity>
+      <TouchableOpacity onPress={() => {
+        RNToasty.Show({ title: "Message", titleSize: 50, titleColor: '#555555', withIcon: true, duration: 1, tintColor: '#000000', icon: copy });
+      }}>
+        <Text>{'Custom'}</Text>
+      </TouchableOpacity>
+    </View>;
   }
 }
 
--- ios/RNToasty.podspec
+++ ios/RNToasty.podspec
@@ -1,24 +1,23 @@
+require 'json'
+
+package = JSON.parse(File.read(File.join(__dir__, '../package.json')))
 
 Pod::Spec.new do |s|
-  s.name         = "RNToasty"
-  s.version      = "1.0.0"
-  s.summary      = "RNToasty"
-  s.description  = <<-DESC
-                  RNToasty
-                   DESC
-  s.homepage     = "https://github.com/author/RNToasty.git"
-  s.license      = "MIT"
-  # s.license      = { :type => "MIT", :file => "FILE_LICENSE" }
-  s.author             = { "author" => "author@domain.cn" }
-  s.platform     = :ios, "7.0"
-  s.source       = { :git => "https://github.com/author/RNToasty.git", :tag => "master" }
-  s.source_files  = "RNToasty/**/*.{h,m}"
-  s.requires_arc = true
+  s.name                   = 'RNToasty'
+  s.version                = package['version']
+  s.summary                = package['description']
+  s.description            = package['description']
+  s.homepage               = package['homepage']
+  s.license                = package['license']
+  s.author                 = package['author']
+  s.source                 = { :git => 'https://github.com/prscX/react-native-toasty.git', :tag => s.version }
 
+  s.platform               = :ios, '9.0'
+  s.ios.deployment_target  = '8.0'
 
-  s.dependency "React"
-  #s.dependency "others"
+  s.preserve_paths         = 'LICENSE', 'package.json'
+  s.source_files           = '**/*.{h,m}'
+  s.dependency             'React'
+  s.dependency             'Toast', '~> 4.0.0'
 
 end
-
-  
