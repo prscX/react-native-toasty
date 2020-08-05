5ee8813eab6e9170b353846ba7c43675db26aaf8
Pranav Chauhan <pranav.chauhan@metricstream.com>
2020-08-05 18:01:00 +0530

    - offsetY default to 50

--- js/RNToasty.js
+++ js/RNToasty.js
@@ -1,10 +1,10 @@
-import PropTypes from 'prop-types';
-import React, { PureComponent } from 'react';
-import { NativeModules, Platform, ViewPropTypes } from 'react-native';
-import Feather from 'react-native-vector-icons/Feather';
-import RNVectorHelper from './RNVectorHelper';
+import PropTypes from 'prop-types'
+import React, { PureComponent } from 'react'
+import { NativeModules, Platform, ViewPropTypes } from 'react-native'
+import Feather from 'react-native-vector-icons/Feather'
+import RNVectorHelper from './RNVectorHelper'
 
-const { RNToasty } = NativeModules;
+const { RNToasty } = NativeModules
 
 class Toasty extends PureComponent {
   static propTypes = {
@@ -26,7 +26,7 @@ class Toasty extends PureComponent {
     position: PropTypes.string,
     offsetX: PropTypes.number,
     offsetY: PropTypes.number,
-  };
+  }
 
   static defaultProps = {
     type: 0,
@@ -42,13 +42,13 @@ class Toasty extends PureComponent {
 
     position: 'bottom',
     offsetX: 0,
-    offsetY: 0,
-  };
+    offsetY: 50,
+  }
 
   static Duration = {
     Short: 0,
     Long: 1,
-  };
+  }
 
   static Types = {
     Normal: 0,
@@ -56,48 +56,46 @@ class Toasty extends PureComponent {
     Success: 2,
     Warn: 3,
     Error: 4,
-  };
+  }
 
   static Show(props) {
-    if (!props) props = {};
-    if (props.type === undefined) props.type = Toasty.defaultProps.type;
+    if (!props) props = {}
+    if (props.type === undefined) props.type = Toasty.defaultProps.type
     // name of font
-    if (props.fontFamily === undefined) props.fontFamily = '';
-    if (props.title === undefined) props.title = Toasty.defaultProps.title;
+    if (props.fontFamily === undefined) props.fontFamily = ''
+    if (props.title === undefined) props.title = Toasty.defaultProps.title
     if (props.titleSize === undefined)
-      props.titleSize = Toasty.defaultProps.titleSize;
+      props.titleSize = Toasty.defaultProps.titleSize
     if (props.titleColor === undefined)
-      props.titleColor = Toasty.defaultProps.titleColor;
+      props.titleColor = Toasty.defaultProps.titleColor
 
     if (props.duration === undefined)
-      props.duration = Toasty.defaultProps.duration;
+      props.duration = Toasty.defaultProps.duration
 
     if (props.tintColor === undefined)
-      props.tintColor = Toasty.defaultProps.tintColor;
+      props.tintColor = Toasty.defaultProps.tintColor
     if (props.withIcon === undefined)
-      props.withIcon = Toasty.defaultProps.withIcon;
+      props.withIcon = Toasty.defaultProps.withIcon
 
     if (props.withIcon) {
       if (props.icon && props.icon.props) {
-        let icon = props.icon.props;
+        let icon = props.icon.props
 
-        let vectorIcon = RNVectorHelper.Resolve(icon.family, icon.name);
-        props.icon = Object.assign({}, icon, vectorIcon);
+        let vectorIcon = RNVectorHelper.Resolve(icon.family, icon.name)
+        props.icon = Object.assign({}, icon, vectorIcon)
       }
     } else {
-      props.icon = undefined;
+      props.icon = undefined
     }
 
     if (props.position === undefined)
-      props.position = Toasty.defaultProps.position;
+      props.position = Toasty.defaultProps.position
 
-    if (props.offsetX === undefined)
-      props.offsetX = Toasty.defaultProps.offsetX;
+    if (props.offsetX === undefined) props.offsetX = Toasty.defaultProps.offsetX
 
-    if (props.offsetY === undefined)
-      props.offsetY = Toasty.defaultProps.offsetY;
+    if (props.offsetY === undefined) props.offsetY = Toasty.defaultProps.offsetY
 
-    RNToasty.Show(props);
+    RNToasty.Show(props)
   }
 
   static successStyle = {
@@ -110,17 +108,17 @@ class Toasty extends PureComponent {
         family={'Feather'}
       />
     ),
-  };
+  }
   static Success(props) {
-    if (!props) props = {};
+    if (!props) props = {}
     if (props.tintColor === undefined && Platform.OS === 'ios')
-      props.tintColor = Toasty.successStyle.tintColor;
+      props.tintColor = Toasty.successStyle.tintColor
     if (props.icon === undefined && Platform.OS === 'ios')
-      props.icon = Toasty.successStyle.icon;
+      props.icon = Toasty.successStyle.icon
 
-    props.type = Toasty.Types.Success;
+    props.type = Toasty.Types.Success
 
-    Toasty.Show(props);
+    Toasty.Show(props)
   }
 
   static errorStyle = {
@@ -133,17 +131,17 @@ class Toasty extends PureComponent {
         family={'Feather'}
       />
     ),
-  };
+  }
   static Error(props) {
-    if (!props) props = {};
+    if (!props) props = {}
     if (props.tintColor === undefined && Platform.OS === 'ios')
-      props.tintColor = Toasty.errorStyle.tintColor;
+      props.tintColor = Toasty.errorStyle.tintColor
     if (props.icon === undefined && Platform.OS === 'ios')
-      props.icon = Toasty.errorStyle.icon;
+      props.icon = Toasty.errorStyle.icon
 
-    props.type = Toasty.Types.Error;
+    props.type = Toasty.Types.Error
 
-    Toasty.Show(props);
+    Toasty.Show(props)
   }
 
   static infoStyle = {
@@ -151,17 +149,17 @@ class Toasty extends PureComponent {
     icon: (
       <Feather name={'info'} size={22} color={'#FFFFFF'} family={'Feather'} />
     ),
-  };
+  }
   static Info(props) {
-    if (!props) props = {};
+    if (!props) props = {}
     if (props.tintColor === undefined && Platform.OS === 'ios')
-      props.tintColor = Toasty.infoStyle.tintColor;
+      props.tintColor = Toasty.infoStyle.tintColor
     if (props.icon === undefined && Platform.OS === 'ios')
-      props.icon = Toasty.infoStyle.icon;
+      props.icon = Toasty.infoStyle.icon
 
-    props.type = Toasty.Types.Info;
+    props.type = Toasty.Types.Info
 
-    Toasty.Show(props);
+    Toasty.Show(props)
   }
 
   static warnStyle = {
@@ -174,30 +172,30 @@ class Toasty extends PureComponent {
         family={'Feather'}
       />
     ),
-  };
+  }
   static Warn(props) {
-    if (!props) props = {};
+    if (!props) props = {}
     if (props.tintColor === undefined && Platform.OS === 'ios')
-      props.tintColor = Toasty.warnStyle.tintColor;
+      props.tintColor = Toasty.warnStyle.tintColor
     if (props.icon === undefined && Platform.OS === 'ios')
-      props.icon = Toasty.warnStyle.icon;
+      props.icon = Toasty.warnStyle.icon
 
-    props.type = Toasty.Types.Warn;
+    props.type = Toasty.Types.Warn
 
-    Toasty.Show(props);
+    Toasty.Show(props)
   }
 
   static normalStyle = {
     tintColor: '#484d51',
-  };
+  }
   static Normal(props) {
-    if (!props) props = {};
+    if (!props) props = {}
     if (props.tintColor === undefined && Platform.OS === 'ios')
-      props.tintColor = Toasty.normalStyle.tintColor;
+      props.tintColor = Toasty.normalStyle.tintColor
 
-    props.type = Toasty.Types.Normal;
+    props.type = Toasty.Types.Normal
 
-    Toasty.Show(props);
+    Toasty.Show(props)
   }
 
   componentDidMount() {}
@@ -207,8 +205,8 @@ class Toasty extends PureComponent {
   show() {}
 
   render() {
-    return null;
+    return null
   }
 }
 
-export { Toasty as RNToasty };
+export { Toasty as RNToasty }
